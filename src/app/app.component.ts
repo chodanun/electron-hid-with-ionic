6609c3c8b9dcc102a140b76effaf77f9dea1d43a
chodanun srinil <chodanun@giinos.com>
2017-07-19 08:54:47 +0700

    start ionic-electron

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+// import * as HID  from 'node-hid';
+// import 'node-hid';
+// declare var HID: any;
+// import * as a from './test';
+// import 'node-hid';
+
+// declare var HID: any;
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -11,12 +20,24 @@ export class MyApp {
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.init();
+      
     });
-  }
+
+    }
+
+    init(){
+      // let x = new HID();
+      // console.log(x);
+      // let devices = HID.devices();
+      // console.log(HID)
+
+    }
 }
 
