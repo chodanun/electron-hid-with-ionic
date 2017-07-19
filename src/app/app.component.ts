19e56135b2b0b2a4f1b5e2597c85d0bf788750b6
chodanun srinil <chodanun@giinos.com>
2017-07-19 14:56:15 +0700

    refactor br_process

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 
 // BARCODE READER
-// declare var HID: any;
+declare var HID: any;
 
 @Component({
   templateUrl: 'app.html'
@@ -15,58 +15,60 @@ export class MyApp {
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    
     platform.ready().then(() => {
-
       statusBar.styleDefault();
       splashScreen.hide();
       this.init();
-      
     });
+  }
 
-    }
+  init(){
+    this.barcode_process();
+  }
 
-    init(){
-      this.getData();
-    }
+  barcode_process(){
+    let barcode_reader = this.getDevice();
+    this.getData(barcode_reader);
+    
+  }
 
-    getData(){
-      let device = getDevice();
-      let productId = device.productId;
-      let vendorId = device.vendorId;
-      let barcode_reader = new HID.HID(vendorId,productId);
+  getData(br){
+    br.on("data", (data) => {
+      // console.log(data);
+      // console.log(data.toString('hex'));
+      let string_data = this.hexToString(data);
+      console.log(string_data);
+      console.log(string_data.length);
+    });
+  }
 
-      barcode_reader.on("data", (data) => {
-        // console.log(data);
-        // console.log(data.toString('hex'));
-        let string_data = this.hexToString(data);
-        console.log(string_data);
-        console.log(string_data.length);
-      });
-    }
+  getDevice(){
+    let devices = HID.devices();
+    let device = devices.filter( device => {
+      return device.serialNumber == "17161B2BEE"
+    })[0];
+    
+    console.log(device);
 
-    getDevice(){
-      let devices = HID.devices();
-      let device = devices.filter( device => {
-        return device.serialNumber == "17161B2BEE"
-      })[0];
-      console.log(device);
-      return device;
-    }
+    let productId = device.productId;
+    let vendorId = device.vendorId;
+    let barcode_reader = new HID.HID(vendorId,productId);
+    return barcode_reader;
+  }
 
-    hexToString (data) {
-      let string = '';
-      let i ; // start byte -> 10
-      let h ;
-      let hex = data.toString('hex')
-      for (i=10; i < hex.length; i += 2) {
-        h = parseInt(hex.substr(i,2), 16);
-        if (h == 13){ // carriage return number
-          return string ;
-        }
-        string += String.fromCharCode(h);
+  hexToString (data) {
+    let string = '';
+    let i ; // start byte -> 10
+    let h ;
+    let hex = data.toString('hex')
+    for (i=10; i < hex.length; i += 2) {
+      h = parseInt(hex.substr(i,2), 16);
+      if (h == 13){ // carriage return number
+        return string ;
       }
-      return string;
+      string += String.fromCharCode(h);
     }
+    return string;
+  }
 }
 
