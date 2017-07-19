a8e0dc06f78ebee46f4a810a84ecccc6b8a5e2a7
chodanun srinil <chodanun@giinos.com>
2017-07-19 18:26:50 +0700

    finished with console log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,17 @@ import { HomePage } from '../pages/home/home';
 import { RestService } from '../shared/rest.service'
 
 // BARCODE READER
-// declare var HID: any;
+declare var HID: any;
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
-  url: string = "/rest/entrypoint/branches/35/entryPoints/2/visits/";
+  mKey: any;
+  test: any = "hello";
+  // url: string = "/rest/entrypoint/branches/35/entryPoints/2/visits/";
+  url: string = "http://192.168.1.92:8080/rest/entrypoint/branches/35/entryPoints/2/visits/";
   body: any = {
     "services" : [14],
     "parameters" : {
@@ -32,22 +35,25 @@ export class MyApp {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
-      // this.init();
-      this.dev();
+      this.init();
+      // this.dev();
     });
   }
 
-  // init(){
-  //   this.barcode_process();
-  // }
+  init(){
+    this.barcode_process();
+  }
 
-  // barcode_process(){
-  //   let barcode_reader = this.getDevice();
-  //   this.getData(barcode_reader);
-  // }
+  barcode_process(){
+    let barcode_reader = this.getDevice();
+    this.getData(barcode_reader);
+  }
 
   postData(barcode_data){
-    this.body.parameters.mKey = barcode_data;
+    this.test = "hello world"
+    this.mKey = barcode_data;
+    console.log("mKey: ",this.mKey)
+    this.body.parameters.mKey = this.mKey;
     console.log(this.body)
     
     this.rest.clearBasicAuthen();
@@ -65,45 +71,45 @@ export class MyApp {
     //   });
   }
 
-  // getData(br){
-  //   br.on("data", (data) => {
-  //     // console.log(data);
-  //     // console.log(data.toString('hex'));
-  //     let string_data = this.hexToString(data);
-  //     console.log(string_data);
-  //     this.postData(string_data);
-  //     // console.log(string_data.length);
-  //   });
-  // }
+  getData(br){
+    br.on("data", (data) => {
+      // console.log(data);
+      // console.log(data.toString('hex'));
+      let string_data = this.hexToString(data);
+      console.log(string_data);
+      this.postData(string_data);
+      // console.log(string_data.length);
+    });
+  }
 
-  // getDevice(){
-  //   let devices = HID.devices();
-  //   let device = devices.filter( device => {
-  //     return device.serialNumber == "17161B2BEE"
-  //   })[0];
+  getDevice(){
+    let devices = HID.devices();
+    let device = devices.filter( device => {
+      return device.serialNumber == "17161B2BEE"
+    })[0];
     
-  //   console.log(device);
+    console.log(device);
 
-  //   let productId = device.productId;
-  //   let vendorId = device.vendorId;
-  //   let barcode_reader = new HID.HID(vendorId,productId);
-  //   return barcode_reader;
-  // }
+    let productId = device.productId;
+    let vendorId = device.vendorId;
+    let barcode_reader = new HID.HID(vendorId,productId);
+    return barcode_reader;
+  }
 
-  // hexToString (data) {
-  //   let string = '';
-  //   let i ; // start byte -> 10
-  //   let h ;
-  //   let hex = data.toString('hex')
-  //   for (i=10; i < hex.length; i += 2) {
-  //     h = parseInt(hex.substr(i,2), 16);
-  //     if (h == 13){ // carriage return number
-  //       return string ;
-  //     }
-  //     string += String.fromCharCode(h);
-  //   }
-  //   return string;
-  // }
+  hexToString (data) {
+    let string = '';
+    let i ; // start byte -> 10
+    let h ;
+    let hex = data.toString('hex')
+    for (i=10; i < hex.length; i += 2) {
+      h = parseInt(hex.substr(i,2), 16);
+      if (h == 13){ // carriage return number
+        return string ;
+      }
+      string += String.fromCharCode(h);
+    }
+    return string;
+  }
 
   dev(){
     this.postData("-KpOuhweuWiM_1ti3K-Z");
