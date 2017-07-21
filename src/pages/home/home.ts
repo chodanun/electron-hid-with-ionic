dabe8033f3b4cb10418c5e0828449007767807d1
chodanun srinil <chodanun@giinos.com>
2017-07-21 16:31:17 +0700

    detect isMobile

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,6 +8,10 @@ import { RestService } from '../../shared/rest.service'
 // BARCODE READER
 declare var HID: any;
 
+// Model
+import { InputModel } from '../../shared/model/inputModel'
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -15,18 +19,21 @@ declare var HID: any;
 export class HomePage {
   mKey: any;
   test: any = "hello";
+  barcodeInputModel: InputModel;
   // url: string = "/rest/entrypoint/branches/35/entryPoints/2/visits/";
   url: string = "http://192.168.1.92:8080/rest/entrypoint/branches/35/entryPoints/2/visits/";
   proxyEventUrl: string= "http://192.168.1.201:8080/ciix-fusion/rest/proxy/events/";
+
   body: any = {
     "services" : [14],
     "parameters" : {
-      "customerName":"visitor noioie223",
-      "isMobile" : "true",
-      "public_key":"sdsdsds",
-      "mKey": "",
+      // "customerName":"",
+      // "isMobile" : "",
+      // "public_key":"sdsdsds",
+      // "mKey": "",
     }
   }
+
   proxyEventBody: any = {
     eventName: "FLOWTOCOL.VISIT_CREATE",
     status: "LISTENING_DATA",
@@ -36,6 +43,7 @@ export class HomePage {
 
   constructor(public navCtrl: NavController, private rest: RestService) {
     this.init();
+    // this.dev();
   }
   
 
@@ -49,16 +57,26 @@ export class HomePage {
   }
 
   postData(barcode_data){
-    this.mKey = barcode_data;
-    this.body.parameters.mKey = this.mKey;
-    
-    this.postToR6(barcode_data);
-    this.postToProxy(barcode_data);
+    try {
+      var inputJSON = JSON.parse(barcode_data);  
+      console.log(inputJSON);
+      this.barcodeInputModel = inputJSON;
+      if (this.barcodeInputModel.isMobile){
+        this.mKey = this.barcodeInputModel.mKey;
+        this.body.parameters = this.barcodeInputModel ;
+        this.postToR6();
+        this.postToProxy();
+      }else{
+        console.log(`it's not "QR-App" barcode`)
+      }
       
+    } catch (error) {
+      console.log("it's not a mobile device or wrong format in json");
+    }
   }
 
-  postToProxy(barcode_data){
-    this.proxyEventBody.mKey = barcode_data;
+  postToProxy(){
+    this.proxyEventBody.mKey = this.barcodeInputModel.mKey;
     // console.log(this.proxyEventBody);
     this.rest.post(this.proxyEventUrl,this.proxyEventBody)
       .then( (data) => {
@@ -66,7 +84,7 @@ export class HomePage {
       });
   }
 
-  postToR6(barcode_data){
+  postToR6(){
     console.log(this.body)
     this.rest.setBasicAuthen(btoa("superadmin:ulan"));
     this.rest.post(this.url,this.body)
@@ -78,11 +96,10 @@ export class HomePage {
 
   getData(br){
     br.on("data", (data) => {
-      // console.log(data);
-      // console.log(data.toString('hex'));
+      console.log(data);
+      console.log(data.toString('hex'));
       let string_data = this.hexToString(data);
       console.log(string_data);
-      this.mKey = string_data ;
       this.postData(string_data);
     });
   }
@@ -90,7 +107,7 @@ export class HomePage {
   getDevice(){
     let devices = HID.devices();
     let device = devices.filter( device => {
-      return device.serialNumber == "17161B2BEE"
+      return device.serialNumber == "17161B2BEE";
     })[0];
     
     console.log(device);
@@ -117,7 +134,7 @@ export class HomePage {
   }
 
   dev(){
-    this.postData("-KpOuhweuWiM_1ti3K-Z");
+    this.postData('{"mKey":"-KpOuhweuWiM_1ti3K-Z","customerName":"Chodanun Srinil","isMobile":"true","other":"test"}');
   }
 
 }
---
+++ src/shared/model/inputModel.ts
@@ -0,0 +1,5 @@
+export class InputModel {
+    isMobile: any;
+    mKey: string;
+    customerName: string;
+}
